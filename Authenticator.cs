f28baacefbed554529fd19628e440a47267f5e8d
Nick Ward <nickward@microsoft.com>
2024-02-16 14:29:39 +1100

    changed to x-ms-client-principal-id header

--- Authenticator.cs
+++ Authenticator.cs
@@ -5,27 +5,25 @@ namespace Hectagon.BookJournal
 {
     public static class Authenticator
     {
-        public static string GetUserId(HttpRequest req)
+        public static string? GetUserId(HttpRequest req)
         {
             string? environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-            ClaimsPrincipal principal = req.HttpContext.User;
+            string? userId = req.Headers["x-ms-client-principal-id"];
 
-            if (environment == "Development" && principal.Identity.IsAuthenticated == false)
+            if (string.IsNullOrEmpty(userId))
             {
-                principal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                if (environment == "Development")
                 {
-                    new Claim(ClaimTypes.Name, "testuser"),
-                    new Claim(ClaimTypes.NameIdentifier, "testuser")
-                }, "TestAuthentication"));
+                    userId = "testuser";
+                }
+                else
+                {
+                    return null;
+                }
             }
 
-            if (principal == null || !principal.Identity.IsAuthenticated)
-            {
-                return null;
-            }
-
-            return principal.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return userId;
         }
     }
 }
